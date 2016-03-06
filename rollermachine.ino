efac033539507250a9844cb53b493ca73479efaf
Nick Ayers <n1ckay3r5@gmail.com>
2016-03-06 22:11:33 +0000

    add servo control

--- rollermachine.ino
+++ rollermachine.ino
@@ -1,3 +1,5 @@
+#include <Servo.h>
+
 /*
 Arduino Hall Effect Sensor Project
 by Arvind Sanjeev
@@ -5,33 +7,44 @@ Please check out  http://diyhacking.com for the tutorial of this project.
 DIY Hacking
 */
 
-
- volatile byte half_revolutions;
+ Servo myservo;
+ volatile byte revolutions;
  unsigned int rpm;
  unsigned long timeold;
+ int pos=0;
+ 
  void setup()
  {
    Serial.begin(9600);
    attachInterrupt(0, magnet_detect, FALLING);//Initialize the intterrupt pin (Arduino digital pin 2)
-   half_revolutions = 0;
+   revolutions = 1;
    rpm = 0;
    timeold = 0;
    pinMode(13, OUTPUT);  
+   myservo.attach(9);
  }
  void loop()//Measure RPM
  {
-   Serial.println("--");
-   delay(1000);
-   if (half_revolutions >= 20) { 
-     rpm = 30*1000/(millis() - timeold)*half_revolutions;
-     timeold = millis();
-     half_revolutions = 0;
-     //Serial.println(rpm,DEC);
-   }
+   Serial.print(revolutions);
+   Serial.print(" - ");
+   Serial.print(millis()-timeold);
+
+   rpm = 1000/(millis() - timeold)*revolutions;
+   timeold = millis();
+   revolutions = 0;
+   
+   Serial.print(" - ");
+   Serial.print(rpm);
+   Serial.print(" - ");
+   Serial.println(rpm/10.0);
+   
+   myservo.write((rpm/10.0)*180);
+   delay(500);
  }
+ 
  void magnet_detect()//This function is called whenever a magnet/interrupt is detected by the arduino
  {
-   half_revolutions++;
+   revolutions++;
    Serial.println("detect");
    digitalWrite(13,HIGH);
    delay(1000);
